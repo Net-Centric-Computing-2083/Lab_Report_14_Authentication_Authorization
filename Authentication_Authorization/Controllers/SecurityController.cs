bcdbc2a1af7d659886246432e73458321a39ba1f
aakritidhungana <aakritidhungana123@gmail.com>
2026-09-18 22:06:10 +0545

    commit

---
+++ Authentication_Authorization/Controllers/SecurityController.cs
@@ -0,0 +1,105 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_Report_14_Authentication_Authorization.Controllers
+{
+    public class SecurityController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public SecurityController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Authorize]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Student")]
+        public IActionResult Student()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> MakeAdmin()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user != null)
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+            }
+
+            return RedirectToAction("Admin");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> MakeStudent()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user != null)
+            {
+                await _userManager.AddToRoleAsync(user, "Student");
+            }
+
+            return RedirectToAction("Student");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> AddDepartmentClaim()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+                return Challenge();
+
+            var claims = await _userManager.GetClaimsAsync(user);
+
+            if (!claims.Any(c => c.Type == "Department"))
+            {
+                await _userManager.AddClaimAsync(
+                    user,
+                    new Claim("Department", "CSIT"));
+            }
+
+            return RedirectToAction("CSITOnly");
+        }
+
+        [Authorize(Policy = "CSITOnly")]
+        public async Task<IActionResult> CSITOnly()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+                return Challenge();
+
+            var claims = await _userManager.GetClaimsAsync(user);
+
+            var department = claims
+                .FirstOrDefault(c => c.Type == "Department")?.Value;
+
+            ViewBag.Department = department;
+
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult PublicPage()
+        {
+            return View();
+        }
+    }
+
+}
--- Authentication_Authorization/Program.cs
+++ Authentication_Authorization/Program.cs
@@ -4,43 +4,65 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("CSITOnly", policy =>
+        policy.RequireClaim("Department", "CSIT"));
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager =
+        scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    string[] roles = { "Admin", "Student" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(
+                new IdentityRole(role));
+        }
+    }
+}
 
 app.Run();
